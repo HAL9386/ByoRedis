e862593d37261843bfafac38e84a6cc9d19bac19
hal <mad024039@gmail.com>
2025-08-14 12:43:40 +0800

    feat: implement resizable HMap with progressive rehashing functionality

--- hashtable.cc
+++ hashtable.cc
@@ -2,6 +2,9 @@
 #include "hashtable.hh"
 #include <assert.h>
 
+size_t const k_max_load_factor = 8;
+size_t const k_rehashing_work = 128;  // how many keys to migrate in one rehashing step
+
 static void round_up_power_of_2(size_t &n) {
   n -= 1;
   n |= n >> 1;
@@ -30,9 +33,8 @@ static void h_insert(HTab *htab, HNode *node) {
   htab->size    += 1;
 }
 
-// the address of the pointer whose value is the address of target node
-// actually return the address of its parent pointer,
-// we need the addresss of the pointer to delete it,
+// actually return the address of its parent pointer(who hold target node's address)
+// either the slot or the prev node's next field
 static HNode ** h_lookup(HTab *htab, HNode *key, bool (*eq)(HNode *, HNode *)) {
   if (!htab->tab) {
     return NULL;
@@ -52,4 +54,76 @@ static HNode * h_detach(HTab *htab, HNode **from) {
   *from = node->next;
   htab->size--;
   return node;
+}
+
+static void hm_trigger_rehashing(HMap *hmap) {
+  hmap->older = hmap->newer;  // (newer, older) <- (new_table, newer)
+  h_init(&hmap->newer, (hmap->newer.mask + 1) * 2);
+  hmap->migrate_pos = 0;
+}
+
+static void hm_help_rehashing(HMap *hmap) {
+  size_t nwork = 0;
+  while (nwork < k_rehashing_work && hmap->older.size > 0) {
+    // find a non-empty slot
+    HNode **from = &hmap->older.tab[hmap->migrate_pos];
+    if (!*from) {
+      hmap->migrate_pos++;
+      continue;  // empty slot
+    }
+    // move the first list item to the newer table
+    h_insert(&hmap->newer, h_detach(&hmap->older, from));
+    nwork++;
+  }
+  // discard the old table if done
+  if (hmap->older.size == 0 && hmap->older.tab) {
+    free(hmap->older.tab);
+    hmap->older = HTab{};
+  }
+}
+
+HNode * hm_lookup(HMap *hmap, HNode *key, bool (*eq)(HNode *, HNode *)) {
+  hm_help_rehashing(hmap);
+  HNode **from = h_lookup(&hmap->newer, key, eq);
+  if (!from) {
+    from = h_lookup(&hmap->older, key, eq);
+  }
+  return from ? *from : NULL;
+}
+
+HNode * hm_delete(HMap *hmap, HNode *key, bool (*eq)(HNode *, HNode *)) {
+  hm_help_rehashing(hmap);
+  if (HNode **from = h_lookup(&hmap->newer, key, eq)) {
+    return h_detach(&hmap->newer, from);
+  }
+  if (HNode **from = h_lookup(&hmap->older, key, eq)) {
+    return h_detach(&hmap->older, from);
+  }
+  return NULL;
+}
+
+// Insertion always update the newer table
+// It triggers rehashing when load factor is high
+void hm_insert(HMap *hmap, HNode *node) {
+  if (!hmap->newer.tab) {
+    h_init(&hmap->newer, 4);     // initialized it if empty
+  }
+  h_insert(&hmap->newer, node);
+  if (!hmap->older.tab) {  // check whether we need to trigger rehash. When older table is not empty, means we are in the rehashing process.
+    size_t shreshold = (hmap->newer.mask + 1) * k_max_load_factor;
+    if (hmap->newer.size >= shreshold) {
+      hm_trigger_rehashing(hmap);
+    }
+  }
+  hm_help_rehashing(hmap);       // migrate some keys
+}
+
+void hm_clear(HMap *hmap) {
+  free(hmap->newer.tab);
+  free(hmap->older.tab);
+  *hmap = HMap{};
+}
+
+size_t hm_size(HMap *hmap) {
+  return hmap->newer.size + hmap->older.size;
 }
--- hashtable.hh
+++ hashtable.hh
@@ -22,4 +22,18 @@ struct HTab {
   size_t size = 0;     // number of keys
 };
 
+// the resizable HMap is based on the fixed-size HTab,
+// contains 2 of them for the progressive rehashing.
+struct HMap {
+  HTab newer;
+  HTab older;
+  size_t migrate_pos = 0;
+};
+
+HNode * hm_lookup(HMap *hmap, HNode *key, bool (*eq)(HNode *, HNode *));
+void    hm_insert(HMap *hmap, HNode *node);
+HNode * hm_delete(HMap *hmap, HNode *key, bool (*eq)(HNode *, HNode *));
+void    hm_clear(HMap *hmap);
+size_t  hm_size(HMap *hmap);
+
 #endif  // HASH_TABLE_HH
